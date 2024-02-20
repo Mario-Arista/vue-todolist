303310c63042f9f67d769f3ff8102172f39125aa
mario.arista <mario.arista.dev@gmail.com>
2024-02-20 13:58:40 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const {createApp} = Vue;
+
+createApp({
+    data() {
+        return {
+
+            // Inserire dati
+            
+
+        }
+    },
+
+    methods: {
+        // Inserire funzioni
+
+    },
+
+}).mount("#app");
